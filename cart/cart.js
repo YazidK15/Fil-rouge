047a6f053dc89456d1037d1c02ded9bcb1e545e3
Yazid <yaazid15@gmail.com>
2024-04-19 14:44:24 +0200

    Ajout de css pour supprimer un article dans le panier

--- cart/cart.js
+++ cart/cart.js
@@ -1,11 +1,17 @@
 var test = document.getElementsByClassName("quantity_value")
 
 function increment (index) {
-test[index].value = parseInt(test[index].value) + 1
+    test[index].value = parseInt(test[index].value) + 1
 }
 
 function decrease (index) {
     if (test[index].value > 0) {
         test[index].value = parseInt(test[index].value) - 1
     }
+}
+
+var test2 = document.getElementsByClassName("cart-item")
+
+function deleteItem (index) {
+    test2[index].style.display = "none";
 }
